98eb3913403ac60bc7d7e5f2bdcd691769b07e82
TobMit <80719513+tobmit@users.noreply.github.com>
2023-02-15 14:55:08 +0100

    Nejaký update na projekte

---
+++ TestUIFramework/TestUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestUI.Core;
+
+namespace TestUI.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            //
+        }
+
+    }
+}
+
