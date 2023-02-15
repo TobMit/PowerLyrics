1144af08173d56ebbb28c3c3ded2d3efd6382251
TobMit <80719513+tobmit@users.noreply.github.com>
2023-02-15 20:23:38 +0100

    View done

---
+++ TestUIFramework/TestUI/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestUI.Core;
+
+namespace TestUI.MVVM.ViewModel
+{
+    internal class HomeViewModel : ObservableObject
+    {
+        public HomeViewModel()
+        {
+            
+        }
+    }
+}
--- TestUIFramework/TestUI/MVVM/ViewModel/MainViewModel.cs
+++ TestUIFramework/TestUI/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,25 @@ namespace TestUI.MVVM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
+
+        public HomeViewModel HomeVm { get; set; }     
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
-            //
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
         }
 
     }
