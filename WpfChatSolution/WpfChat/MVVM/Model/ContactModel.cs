3272ba87b753c5eb2832b5955e7011714f5defd6
TobMit <80719513+tobmit@users.noreply.github.com>
2023-02-16 20:26:07 +0100

    Contacts a modely správ hotove

---
+++ WpfChatSolution/WpfChat/MVVM/Model/ContactModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfChat.MVVM.Model
+{
+    internal class ContactModel
+    {
+        public string Username { get; set; }
+        public string ImageSource { get; set; }
+
+        public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public string LastMessage => Messages.Last().Message;
+    }
+}
--- WpfChatSolution/WpfChat/MVVM/ViewModel/MainViewModel.cs
+++ WpfChatSolution/WpfChat/MVVM/ViewModel/MainViewModel.cs
@@ -1,16 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfChat.MVVM.Model;
 
 namespace WpfChat.MVVM.ViewModel
 {
     internal class MainViewModel
     {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
         public MainViewModel()
         {
-            
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            //ideme naplnit kontajnery len pre viditelnost
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Alison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://images.alphacoders.com/475/475526.jpg",
+                Message = "Test n: 0",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Alison",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://images.alphacoders.com/475/475526.jpg",
+                    Message = "Test n: " + i + 1,
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://images.alphacoders.com/475/475526.jpg",
+                    Message = "Test n: " + i + 1,
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true,
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Bunny",
+                UsernameColor = "#409aff",
+                ImageSource = "https://images.alphacoders.com/475/475526.jpg",
+                Message = "Last",
+                Time = DateTime.Now,
+                IsNativeOrigin = true,
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    // toto je string interpolation
+                    Username = $"Allison {i}",
+                    ImageSource = "https://rare-gallery.com/uploads/posts/536732-anonymous-wallpaper.jpg",
+                    Messages = Messages
+                });
+            }
         }
     }
 }
