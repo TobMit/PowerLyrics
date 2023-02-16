4c0f0c915aad40e4bb0311fff4688bb21f49840c
TobMit <80719513+tobmit@users.noreply.github.com>
2023-02-16 19:43:08 +0100

    Vrch okna hotový

---
+++ WpfChatSolution/WpfChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfChat.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
