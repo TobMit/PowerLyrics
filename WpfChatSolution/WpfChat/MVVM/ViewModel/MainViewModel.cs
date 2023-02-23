40b9fa5dfd589f455672312d11ebbf4145b05ba2
TobMit <80719513+tobmit@users.noreply.github.com>
2023-02-23 19:43:38 +0100

    Takmer hotove

--- WpfChatSolution/WpfChat/MVVM/ViewModel/MainViewModel.cs
+++ WpfChatSolution/WpfChat/MVVM/ViewModel/MainViewModel.cs
@@ -4,19 +4,60 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfChat.Core;
 using WpfChat.MVVM.Model;
 
 namespace WpfChat.MVVM.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                _selectedContact = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+                Message = "";
+            });
 
             //ideme naplnit kontajnery len pre viditelnost
 
