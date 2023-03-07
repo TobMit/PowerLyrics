4389f4218792100238b533d6df833feaf6d1bdf9
TobMit <80719513+tobmit@users.noreply.github.com>
2023-03-07 19:18:06 +0100

    Move Windows into correct folder

--- PowerLyricsSolution/PowerLyrics/Windows/MainWindow.xaml.cs
+++ PowerLyricsSolution/PowerLyrics/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PowerLyrics
+namespace PowerLyrics.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
