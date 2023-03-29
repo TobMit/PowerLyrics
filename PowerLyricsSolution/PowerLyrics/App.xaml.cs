2d1bc58044e9b03fbe208cb5539995b114fda3de
TobMit <80719513+tobmit@users.noreply.github.com>
2023-03-30 00:26:51 +0200

    App crash Handler

--- PowerLyricsSolution/PowerLyrics/App.xaml.cs
+++ PowerLyricsSolution/PowerLyrics/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace PowerLyrics
 {
@@ -13,5 +14,18 @@ namespace PowerLyrics
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+        }
+
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            MessageBox.Show("We are sorry for crashing. This app is still in progress!", "App crash", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
+    
+
 }
