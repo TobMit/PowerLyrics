029cd1af965e14675ba6b83ae99bb693064fc898
TobMit <80719513+tobmit@users.noreply.github.com>
2024-07-15 23:41:02 +0200

    [NewCore] - PlaylistCore init

---
+++ PowerLyricsSolution/PowerLyrics/Core/PresentingCore/PlaylistCore/PlayListCore.cs
@@ -0,0 +1,16 @@
+﻿using PowerLyrics.MVVM.ViewModel;
+
+namespace PowerLyrics.Core.PresentingCore.PlaylistCore;
+
+/// <summary>
+/// Playlist core handle all the playlist operations
+/// </summary>
+public class PlayListCore
+{
+    private PresentingViewModel _pPresentingViewModel;
+
+    public PlayListCore(PresentingViewModel pPresentingViewModel)
+    {
+        _pPresentingViewModel = pPresentingViewModel;
+    }
+}
