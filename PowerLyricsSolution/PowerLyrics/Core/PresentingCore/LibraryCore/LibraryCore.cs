a337c671b7a3590dd8905202b813ab474001ed5c
TobMit <80719513+tobmit@users.noreply.github.com>
2024-07-15 22:34:30 +0200

    [NewCore] - Add LibraryCore

---
+++ PowerLyricsSolution/PowerLyrics/Core/PresentingCore/LibraryCore/LibraryCore.cs
@@ -0,0 +1,25 @@
+﻿using PowerLyrics.Core.DataHandler;
+using PowerLyrics.MVVM.ViewModel;
+
+namespace PowerLyrics.Core.PresentingCore.LibraryCore;
+
+/// <summary>
+/// Library core handle all the library operations
+/// </summary>
+public class LibraryCore
+{
+    private PresentingViewModel _presentingViewModel;
+    public LibraryCore(PresentingViewModel pPresentingViewModel)
+    {
+        _presentingViewModel = pPresentingViewModel;
+    }
+
+    /// <summary>
+    /// Loads library from the Song folder and process them with data loader
+    /// </summary>
+    /// <param name="pLoader">Load the library</param>
+    public void InitLibrary(DataLoader pLoader)
+    {
+        _presentingViewModel.ListOfSongs = pLoader.GetSongs();
+    }
+}
